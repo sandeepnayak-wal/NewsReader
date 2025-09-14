b76b93fcf50cc653d2faab006cf9cda46d9d459b
sandeepnayak-wal <sandeep.megavath@veltris.com>
2025-09-14 11:24:07 +0530

    Initial Commit

---
+++ NewsReaderTests/NewsReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderTests.swift
+//  NewsReaderTests
+//
+//  Created by Sandeep on 14/09/25.
+//
+
+import Testing
+@testable import NewsReader
+
+struct NewsReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
